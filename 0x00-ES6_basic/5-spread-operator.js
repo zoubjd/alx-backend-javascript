f58f4560db2c362ea2da6689a3d935bc290c20fd
zoubjd <zouhairbajdouri19@gmail.com>
2024-10-05 01:55:16 +0100

    up to 7

--- 0x00-ES6_basic/5-spread-operator.js
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -1,7 +1,4 @@
 export default function concatArrays(array1, array2, string) {
-    let retunvalue = array1.concat(array2)
-    for(let i = 0; i < string.length; i++){
-        retunvalue.push(string[i])
-    }
-    return retunvalue;
+  const retunvalue = [...array1, ...array2, ...string];
+  return retunvalue;
 }
--- 0x00-ES6_basic/7-getBudgetObject.js
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -1,9 +1,9 @@
 export default function getBudgetObject(income, gdp, capita) {
-    const budget = {
-    income,
-    gdp,
-    capita,
-    };
-  
-    return budget;
-  }
+  const budget = {
+    income: income,
+    gdp: gdp,
+    capita: capita,
+  };
+
+  return budget;
+}
