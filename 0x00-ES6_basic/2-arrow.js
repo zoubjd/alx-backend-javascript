5e49497efef3881b244c363d6a8b2b25900a5ea2
zoubjd <zouhairbajdouri19@gmail.com>
2024-10-05 01:49:28 +0100

    fixed up to foor

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,10 +1,10 @@
 export default function getNeighborhoodsList() {
-    this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
-  
-    const self = this;
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
-    this.addNeighborhood = (newNeighborhood) => {
-        self.sanFranciscoNeighborhoods.push(newNeighborhood);
-      return self.sanFranciscoNeighborhoods;
-    };
-  }
+  const self = this;
+
+  this.addNeighborhood = (newNeighborhood) => {
+    self.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return self.sanFranciscoNeighborhoods;
+  };
+}
