d6cf3d344c367ba57ee9a58de2ffc886088fc2f6
zoubjd <zouhairbajdouri19@gmail.com>
2024-10-09 00:03:09 +0100

    forgot the readme

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,4 +1,8 @@
 export default function updateUniqueItems(map) {
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+
   map.forEach((value, key) => {
     if (value === 1) {
       map.set(key, 100);
